639865e8913abc78fb9311f177f0cb8f721997ed
Joey Murphy <murphyjm@stanford.edu>
2016-05-17 18:20:45 -0700

    Still working on the calculator. Stuck on how to handle negative signs.

--- calc.py
+++ calc.py
@@ -9,24 +9,38 @@ import sys
 def main():
     """Join command-line arguments and pass them to unitcalc(), then print."""
     calculation = ''.join(sys.argv[1:])
-    print calc(calculation)
+    print(calc(calculation))
 
 def calc(s):
     """Parse a string describing an operation on quantities with units."""
+    ops = []
+    afterOp = False
+    # Iterate over the string from start to finish to find out which operators are contained in the expression. Store them in the ops list.
+    # Takes care not to count leading negative signs on numbers as operators as well as passing over decimal points.
+    for i in range(0,len(s)):
+        if not s[i].isdigit() and not s[i] == '.':
+            if i==0 and s[i] == '-':
+                continue
+            if afterOp and s[i] == '-':
+                continue
+            ops.append(s[i])
+            afterOp = True
+        else:
+            afterOp = False
 
-    # TODO make this robust for differently formatted inputs
-    num1 = s[0]
-    num2 = s[2]
-    operation = s[1]
-
-    if operation=='+':
-        return int(num1)+int(num2)
-    elif operation=='-':
-        pass
-    elif operation=='*':
-        pass
-    elif operation=='/':
-        pass
+    nums = s.split('+ | \* | / | -') # Split the string at the various operators '+', '/', and '*'.
+                                     # Doesn't split on the minus sign '-' since it could be part of a negative number
+    nums = [float(num) for num in nums]
+    result = 0
+    for operation in ops:
+        if operation=='+':
+            result += 
+        elif operation=='-':
+            pass
+        elif operation=='*':
+            pass
+        elif operation=='/':
+            pass
 
 
 if __name__ == "__main__": main()
--- testing.py
+++ testing.py
@@ -9,17 +9,28 @@ import calc
 
 class CalcTest(unittest.TestCase):
     # TODO implement tests here to verify that your functions work!
+    # TODO: def testSyntax(self):
+
     def testAddition(self):
         self.assertEqual(calc.calc('1+1'), 2)
 
+    def testFloats(self):
+        self.assertEqual(calc.calc('2.5+1.4'), 3.9)
+
+    def testMultiDigit(self):
+        self.assertEqual(calc.calc('15+512'), 527)
+
     def testSubtraction(self):
-        pass
+        self.assertEqual(calc.calc('3-1'), 2)
+
+    def testNegativeSum(self):
+        self.assertEqual(calc.calc('3-4', -1))
 
     def testMultiplciation(self):
-        pass
+        self.assertEqual(calc.calc('2*4'), 8)
  
     def testDivision(self):
-        pass
+        self.assertEqual(calc.calc('6/3'), 2)
 
 if __name__ == '__main__':
     unittest.main()
